a5ba9a0845226189cae665ebcf946c86ea161a7f
Victoria Malie <vmalie3@gmail.com>
2022-06-26 15:48:39 -0500

    fixed seeds issues

--- db/schema.sql
+++ db/schema.sql
@@ -25,4 +25,4 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id)
     REFERENCES role(id),
     manager_id INT
-)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,30 @@
 /* Used values from README to have starter data */
 
+USE employee_data;
+
 INSERT INTO department (name)
-VALUES (Sales), (Engineering), (Finance), (Legal);
+VALUES ('Sales'), ('Engineering'), ('Finance'), ('Legal');
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
-    (Sales Lead, 100000, 1),
-    (Salesperson, 80000, 1),
-    (Lead Engineer, 150000, 2),
-    (Software Engineer, 120000, 2),
-    (Account Manager, 160000, 3),
-    (Accountant, 125000, 3),
-    (Legal Team Lead, 250000, 4),
-    (Lawyer, 190000, 4)
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4)
 ;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-    (John, Doe, 1, ),
-    (Mike, Chan, 2, 1),
-    (Ashley, Rodriguez, 3, ),
-    (Kevin, Tupik, 4, 3),
-    (Kunal, Singh, 5, ),
-    (Malia, Brown, 6, 5),
-    (Sarah, Lourd, 7, ),
-    (Tom, Allen, 8, 7)
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7)
 ;
