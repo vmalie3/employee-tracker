0c6d2e5bcda3ce522efd24224a7483b0db5abd89
Victoria Malie <vmalie3@gmail.com>
2022-06-26 16:49:23 -0500

    got add department function to work

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ const db = mysql.createConnection(
     }
 );
 
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
 const allFunctions = {
     viewAllDepartments() {
         db.query('SELECT * FROM department', (err, results) => {
@@ -41,6 +36,24 @@ const allFunctions = {
             return init();
         });
     },
+    // WHEN I choose to add a department
+    // THEN I am prompted to enter the name of the department and that department is added to the database
+    addDepartment() {
+        inquirer.prompt(
+            {
+                type: 'input',
+                message: 'What is the department\'s name?',
+                name: 'newDepartment'
+            }
+        ).then((answer) => {
+            const command = `INSERT INTO department (name) VALUES ('${answer.newDepartment}'); `;
+            db.query(command.toString(), (err, results) => {
+                if (err) console.error(err);
+                console.table(results);
+                return init();
+            });
+        });
+    },
 }
 
 const init = () => {
@@ -48,7 +61,7 @@ const init = () => {
         {name: 'View All Departments', value: 'viewAllDepartments'},
         {name: 'View All roles', value: 'viewAllRoles'},
         {name: 'View All Employees', value: 'viewAllEmployees'},
-        // {name: 'Add a Deparment', value: 'addDepartment'},
+        {name: 'Add a Deparment', value: 'addDepartment'},
         // {name: 'Add a Role', value: 'addRole'},
         // {name: 'Add an Employee', value: 'addEmployee'},
         // {name: 'Update an Employee Role', value: 'updateRole'},
@@ -66,11 +79,6 @@ const init = () => {
 
 init();
 
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
 
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
